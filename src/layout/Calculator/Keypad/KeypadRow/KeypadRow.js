4e386ea1091423d7e011e3fe7c3768da38110fb8
Christine Goddard <christine.goddard@socsci.ox.ac.uk>
2020-01-21 13:13:22 +0000

    Calculator code

---
+++ src/layout/Calculator/Keypad/KeypadRow/KeypadRow.js
@@ -0,0 +1,12 @@
+//KeyPad Row
+
+import React from 'react';
+
+const keypadRow=  (props) => (
+	<div className="keypad_row">
+	{props.children}
+	</div>
+
+	);
+
+export default keypadRow;
---
+++ src/layout/Calculator/Screen/ResultScreen/ResultScreen.js
@@ -0,0 +1,12 @@
+//Results Screen
+
+import React from 'react';
+
+const resultScreen = (props) => (
+ <div className="result-screen">
+ {props.children}
+ </div>
+
+	);
+
+	export default resultScreen;
